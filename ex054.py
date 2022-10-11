3a8f205f1fd342e3cded255616a007d0542efcf5
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-10 23:55:17 -0300

    Update ex054.py

--- ex054.py
+++ ex054.py
@@ -1,11 +1,14 @@
 # pythonexercios
 # conferencia de maior idade.
 from datetime import date
-lista = []
-for c in range(0, 7):
+maior = []
+menor = []
+for c in range(0, 4):
     nasc = int(input('Digite o seu ano de nascimento: '))
     ano = date.today().year
     idade = ano - nasc
-    if idade >= 18:
-        lista.append(idade)
-print(lista)
+    if idade >= 21:
+        maior.append(idade)
+    else:
+        menor.append(idade)
+print('A {} pessoa com idade maior a 21 anos esta são {}'.format(len(maior), maior))
