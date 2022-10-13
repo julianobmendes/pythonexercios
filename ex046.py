a293b04e08a19bdd58d82700c81f940501cff089
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 20:11:35 -0300

    Update ex046.py

--- ex046.py
+++ ex046.py
@@ -1,8 +1,9 @@
+# pythonexercios
+# Contagem regreciva.
 from time import sleep
-
 print('Contagem Regreciva para estouros de fogos:')
 sleep(2)
-for c in range(10, 0, -1):
+for c in range(10, -1, -1):
     print(c)
-    sleep(1)
+    sleep(1.5)
 print('Estouro de fogos')
