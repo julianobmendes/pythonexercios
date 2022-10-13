69b832cbf9d13a23b79e7e335742f9c3570e0475
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 00:19:33 -0300

    Create ex056.py

---
+++ ex056.py
@@ -0,0 +1,11 @@
+# pythonexercios
+# leia o nome, idade e sexo de 4 pessoas.
+'''
+No final do programa, mostre: a média de idade do grupo, 
+qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
+'''
+nome = []
+idade = []
+sexo = []
+for c in range (0, 4):
+    
