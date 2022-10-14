efa54f82003ad44386580c681c27aab82701811f
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:11:58 -0300

    Update ex048.py

--- ex048.py
+++ ex048.py
@@ -1,5 +1,7 @@
 # pythonexercios
 # Exercício 48 – Soma ímpares múltiplos de três.
+'''Faça um programa que calcule a soma entre todos os números que
+são múltiplos de três e que se encontram no intervalo de 1 até 500.'''
 soma = 0
 cont = 0
 for c in range(1, 501, 2):
