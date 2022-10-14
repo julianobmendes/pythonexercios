8a786581eac7983c3067325d57b6bded22d3fa40
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:11:00 -0300

    Update ex047.py

--- ex047.py
+++ ex047.py
@@ -1,4 +1,6 @@
 # pythonexercios
 # Exercícios 47 – Contagem de pares.
+'''Crie um programa que mostre na tela todos os
+números pares que estão no intervalo entre 1 e 50.'''
 for c in range(1, 50, 2):
     print(c+1)
