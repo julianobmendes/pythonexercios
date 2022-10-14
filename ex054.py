b6feebdcde9d0037f34c39f84af967c29dc15fe3
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:27:16 -0300

    Update ex054.py

--- ex054.py
+++ ex054.py
@@ -1,5 +1,8 @@
 # pythonexercios
 # Exercício 54 – Grupo da Maioridade
+'''Crie um programa que leia o ano de nascimento de sete pessoas.
+No final, mostre quantas pessoas ainda não atingiram a maioridade
+e quantas já são maiores.'''
 from datetime import date
 maior = []
 menor = []
