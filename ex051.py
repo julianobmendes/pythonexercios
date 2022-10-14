9b5cddc0aaf4b9197abc97f5d3a5c135ff394c03
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:21:15 -0300

    Update ex051.py

--- ex051.py
+++ ex051.py
@@ -1,5 +1,7 @@
 # pythonexercios
 # Exercício 51 – Progressão Aritmética
+'''Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
+No final, mostre os 10 primeiros termos dessa progressão.'''
 primeiro = int(input("Primeiro elemento: "))
 razao = int(input("Razao: "))
 n = 10
