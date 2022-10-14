13d26e86dcc057781edd40bb9593b67f2247a98a
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:32:17 -0300

    Update ex046.py

--- ex046.py
+++ ex046.py
@@ -1,5 +1,8 @@
 # pythonexercios
-# Contagem regreciva.
+# Exercício 46 – Contagem regressiva
+'''Faça um programa que mostre na tela uma contagem regressiva para
+o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles
+'''
 from time import sleep
 print('Contagem Regreciva para estouros de fogos:')
 sleep(2)
