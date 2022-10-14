ff792d01ecef21740b32b0c633558e488a347ed3
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 21:50:54 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -1,7 +1,8 @@
 # pythonexercios
-# leia o nome, idade e sexo de 4 pessoas.
+# Exercício 56 – Analisador completo
 '''
-No final do programa, mostre: a média de idade do grupo, 
+Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas.
+No final do programa, mostre: a média de idade do grupo,
 qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 '''
 nome = []
