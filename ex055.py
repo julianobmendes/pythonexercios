4cf7a5bcea3caff826539adfa5fd729c6fb6e3ea
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 21:49:11 -0300

    Update ex055.py

--- ex055.py
+++ ex055.py
@@ -1,5 +1,7 @@
 # pythonexercios
-# Leia o peso de 5 pessoas e diga qual é o maior e o menor peso
+# Exercício 55 – Maior e menor da sequência
+'''Faça um programa que leia o peso de cinco pessoas.
+No final, mostre qual foi o maior e o menor peso lidos.'''
 maior = 0
 menor = 0
 for c in range (0, 5):
