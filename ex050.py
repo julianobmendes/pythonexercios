2f5b0ecfd84ab5c6435816d6a9928f087085ad83
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:20:31 -0300

    Update ex050.py

--- ex050.py
+++ ex050.py
@@ -1,5 +1,8 @@
 # pythonexercios
 # Exercício 50 – Soma dos pares
+'''Desenvolva um programa que leia seis números inteiros e mostre
+a soma apenas daqueles que forem pares.
+Se o valor digitado for ímpar, desconsidere-o.'''
 n2 = int(0)
 for c in range(0, 6):
     n1 = int(input('Digite um número: '))
