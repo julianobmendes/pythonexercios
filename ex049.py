653f2488ab9cf8c1e316e57ea148df463630846c
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:14:09 -0300

    Update ex049.py

--- ex049.py
+++ ex049.py
@@ -1,5 +1,7 @@
 # pythonexercios
-# Exercício 49 – Tabuada v.2.0.
+# Exercício 49 – Tabuada v.2.0
+'''Refaça o DESAFIO 9, mostrando a tabuada de um número
+que o usuário escolher, só que agora utilizando um laço for.'''
 n1 = int(input('Digite um número: '))
 n2 = 1
 print('A Tabuada para o numero {} é:'.format(n1))
