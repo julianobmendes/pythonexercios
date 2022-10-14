4720f31998d97880593dd545f5ae898a27c69720
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-13 22:26:15 -0300

    Update ex053.py

--- ex053.py
+++ ex053.py
@@ -1,5 +1,7 @@
 # pythonexercios
 # Exercício 53 – Detector de Palíndromo
+'''Crie um programa que leia uma frase qualquer e diga
+se ela é um palíndromo, desconsiderando os espaços'''
 plvr = str(input('Digite uma palavra: ').upper().replace(' ',''))
   if plvr == plvr[:: -1]:
     print('A palavra é Palindroma','\n')
