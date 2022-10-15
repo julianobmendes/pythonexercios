daac3811aaf59101b986cfe0a951abe762529bdb
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-15 00:13:48 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -8,13 +8,17 @@ qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 nome = []
 idade = []
 sexo = []
-for c in range (0, 4):
-    nom = str(input('Digite o nome da {}º pessoa: '.format(c+1))).strip()
-    nome.append(nom)
-    idad = str(input('Digite a idade da {}º pessoa: '.format(c+1))).strip()
-    idade.append(idad)
+for c in range (0, 2):
+    i_nome = str(input('Digite o nome da {}º pessoa: '.format(c+1))).strip()
+    nome.append(i_nome)
+    i_idade = int(input('Digite a idade da {}º pessoa: '.format(c+1)))
+    idade.append(i_idade)
     print('Digite o sexo da {}º pessoa: '.format(c+1))
     print('[M] para Masculino e [F] para Feminino')
-    sex = str(input('Digite o sexo da {}º pessoa: '.format(c+1))).strip()
-    sexo.append(sex)
-print(nome[0])   
+    i_sexo = str(input()).upper()
+    sexo.append(i_sexo)
+m_idade = (sum(idade) / 4)
+print(nome)
+print(idade)
+print('{:.0f}'.format(m_idade))
+print('{}'.format(sexo.count('M')))
