e68b058cfc775f1f7764d69b292bdd738d6259f5
Juliano Boaventura Mendes <julianomendes_@hotmail.com>
2022-10-16 17:29:44 -0300

    Update ex056.py

--- ex053.py
+++ ex053.py
@@ -1,21 +1,19 @@
 # pythonexercios
 # Exercício 53 – Detector de Palíndromo
-'''Crie um programa que leia uma frase qualquer e diga
-se ela é um palíndromo, desconsiderando os espaços'''
-plvr = str(input('Digite uma palavra: ').upper().replace(' ',''))
-  if plvr == plvr[:: -1]:
-    print('A palavra é Palindroma','\n')
-    print('Palavra Invertida: {}'.format(plvr),'\n')
-  else:
-    print('A palavra não é Palindroma','\n')
-print('Palavra Digitada: {} '.format(plvr),'\n')
-'''pali = input('Digite um palavra: ').upper() #coloca afrse toda em letras maiusculas
+"""Crie um programa que leia uma frase qualquer e diga
+se ela é um palíndromo, desconsiderando os espaços"""
+plvr = str(input('Digite uma palavra: ').upper().replace(' ', ''))
+if plvr == plvr[:: -1]:
+    print('A palavra é Palindroma', '\n')
+    print('Palavra Invertida: {}'.format(plvr), '\n')
+else:
+    print('A palavra não é Palindroma', '\n')
+print('Palavra Digitada: {} '.format(plvr), '\n')
+"""pali = input('Digite um palavra: ').upper() #coloca afrse toda em letras maiusculas
 
-frase_dividida = pali.split()#usamos o .split para separar a frase em espaços
-frase_junta = ''.join(frase_dividida)#o join sera usado para juntar as palavras q o .split separou, lembre-se o join separa as palavras pelos espaços
-frase_invertida = ''
-for cont in range(len(frase_junta) -1, -1, -1):
-    frase_invertida += frase_junta[cont]
+frase_dividida = pali.split()#usamos o .split para separar a frase em espaços frase_junta = ''.join(frase_dividida)
+#o join sera usado para juntar as palavras q o .split separou, lembre-se o join separa as palavras pelos espaços 
+frase_invertida = '' for cont in range(len(frase_junta) -1, -1, -1): frase_invertida += frase_junta[cont] 
 
 print(f'Palavra Junta: {frase_junta}')
-print(f'Palavra Invertida: {frase_invertida}')'''
+print(f'Palavra Invertida: {frase_invertida}')"""
