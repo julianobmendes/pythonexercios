799fab332dcbc70764bce4c2a313c16b81c94baa
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-16 19:09:38 -0300

    Update ex056.py

--- ex056.py
+++ ex056.py
@@ -8,7 +8,10 @@ qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
 nome = []
 idade = []
 sexo = []
-for c in range (0, 2):
+i_homem = 0
+n_homem = 0
+cont = 0
+for c in range(0, 4):
     i_nome = str(input('Digite o nome da {}º pessoa: '.format(c+1))).strip()
     nome.append(i_nome)
     i_idade = int(input('Digite a idade da {}º pessoa: '.format(c+1)))
@@ -18,7 +21,14 @@ for c in range (0, 2):
     i_sexo = str(input()).upper()
     sexo.append(i_sexo)
 m_idade = (sum(idade) / 4)
-print(nome)
-print(idade)
-print('{:.0f}'.format(m_idade))
-print('{}'.format(sexo.count('M')))
+for i in range(0, 4):
+    if sexo[i] == 'M' and idade[i] > i_homem:
+        i_homem = idade[i]
+        n_homem = nome[i]
+for m in range(0, 4):
+    if sexo[m] == 'F' and idade[m] < 21:
+        cont = cont + 1
+print('O grupo é composto por {} homens e {} mulheres.'.format(sexo.count('M'), sexo.count('F')))
+print('A média de idade do grupo é de {:.0f} anos.'.format(m_idade))
+print('O nome do homem mais velho é {} e ele tem {} anos.'.format(n_homem, i_homem))
+print('A {} mulhere(s) com 20 anos ou menos.'.format(cont))
