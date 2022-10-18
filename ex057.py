5888cc2d860820848817115cff0523acb6c6908e
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-17 22:06:32 -0300

    Create ex057.py

---
+++ ex057.py
@@ -0,0 +1,6 @@
+# pythonexercios
+# Exercício 57 – Validação de Dados
+"""
+Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
+Caso esteja errado, peça a digitação novamente até ter um valor correto.
+"""
