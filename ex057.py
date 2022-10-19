25272aaf8f4f61ad6d69e5cb1b9e1984e2f40004
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-19 20:22:07 -0300

    Update ex057.py

--- ex057.py
+++ ex057.py
@@ -4,3 +4,21 @@
 Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’.
 Caso esteja errado, peça a digitação novamente até ter um valor correto.
 """
+sexo = 1
+print('---=== Menu ===---')
+print('Para sexo masculino digite [M]')
+print('Para sexo feminino digite [F]')
+print('Para sair digite [0] zero')
+print('----------------------------------')
+while sexo != '0':
+    sexo = input('Digite o sexo da pessoa: ').upper()
+    if sexo == 'M':
+        print('A pessoa é do sexo Masculino')
+        print('----------------------------')
+    elif sexo == 'F':
+        print('A pesoa é do sexo Feminino')
+        print('----------------------------')
+    elif sexo != '0':
+        print('Você digitou um valor errado, favor repitir')
+        print('-------------------------------------------')
+print('Fim do programa')
