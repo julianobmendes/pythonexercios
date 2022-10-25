84bd1ba61f862f660d98022167cb3026298254c5
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-24 23:56:39 -0300

    Update ex062.py

--- ex062.py
+++ ex062.py
@@ -4,3 +4,12 @@
 Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
 O programa encerrará quando ele disser que quer mostrar 0 termos.
 """
+n = 10
+while n != 0:
+    if n != 0:
+        primeiro = int(input("Primeiro elemento: "))
+        razao = int(input("Razao: "))
+        ultimo = (primeiro + (n-1)*razao) + 1
+        for var in range(primeiro, ultimo, razao):
+            print('{}, '.format(var), end="")
+        print('\n')
