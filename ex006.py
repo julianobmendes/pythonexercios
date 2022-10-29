12d9415196d9ce1fbf805b7d38aa5905e0ceb519
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-28 21:33:06 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,5 +1,7 @@
+# pythonexercios
+# Exercício 6 – Dobro, Triplo, Raiz Quadrada
+"""
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+"""
 n1 = float(input('Digite um numero! '))
-#n2 = int(n1 * 2)
-#n3 = int(n1 * 3)
-#n4 = n1 ** 0.5
 print('O numero apresentado é {} e seu\nDobro é {}\nTriplo é {}\nRaiz quadrada é {:.2f}'.format(n1, n1*2, n1*3, n1**0.5))
