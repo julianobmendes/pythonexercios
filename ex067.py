2459d6c628a671125c38271d064f14a5613265cd
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-28 23:18:27 -0300

    Create ex067.py

---
+++ ex067.py
@@ -0,0 +1,7 @@
+# pythonexercios
+# Exercício 67 – Tabuada v3.0
+"""
+Faça um programa que mostre a tabuada de vários números,
+um de cada vez, para cada valor digitado pelo usuário.
+O programa será interrompido quando o número solicitado for negativo.
+"""
