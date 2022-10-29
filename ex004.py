aac81a2e6c4fadc253f10c14a538348b2e058d92
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-28 21:16:24 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,4 +1,9 @@
+# pythonexercios
+# Exercício 4 – Dissecando uma Variável
+"""
+Faça um programa que leia algo pelo teclado e mostre na tela
+o seu tipo primitivo e todas as informações possíveis sobre ele.
+"""
 n1 = input('Digite qual quer coisa! ')
 resp = n1.isnumeric('É um número')
 print(resp)
-
