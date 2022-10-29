99411b6fa6b52ffeaab0791e789d67f5a2b76f43
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-28 21:44:01 -0300

    Update ex010.py

--- ex010.py
+++ ex010.py
@@ -1,3 +1,9 @@
+# pythonexercios
+# Exercício 10 – Conversor de Moedas
+"""
+Crie um programa que leia quanto dinheiro uma pessoa tem na
+carteira e mostre quantos dólares ela pode comprar.
+"""
 n1 = float(input('Quantos de Dinheiro para cambio: R$ '))
 us = n1 / 3.27
 print('Voce pode comprar US$ {:.2f} dolares com R$ {} reais que voce tem'.format(us, n1))
