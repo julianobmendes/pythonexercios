190d7f0edb1a705f953835bbd5f88379968d9653
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-28 22:27:45 -0300

    Update ex041.py

--- ex041.py
+++ ex041.py
@@ -1,3 +1,15 @@
+# pythonexercios
+# Exercício 41 – Classificando Atletas
+"""
+A Confederação Nacional de Natação precisa de um
+programa que leia o ano de nascimento de um atleta e
+mostre sua categoria, de acordo com a idade:
+Até 9 anos: MIRIM
+Até 14 anos: INFANTIL
+Até 19 anos: JÚNIOR
+Até 25 anos: SÊNIOR
+Acima de 25 anos: MASTER
+"""
 from datetime import date
 ano = int(input('Qual o ano de seu nascimento: '))
 idade = date.today().year - ano
