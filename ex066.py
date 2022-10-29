e1912c6db8f18aeb29717573fe22c77bd5011d37
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-28 23:17:20 -0300

    Create ex066.py

---
+++ ex066.py
@@ -0,0 +1,9 @@
+# pythonexercios
+# Exercício 66 – Vários números com flag
+"""
+Crie um programa que leia números inteiros pelo teclado.
+O programa só vai parar quando o usuário digitar o valor 999,
+que é a condição de parada.
+No final, mostre quantos números foram digitados e
+qual foi a soma entre elas (desconsiderando o flag).
+"""
