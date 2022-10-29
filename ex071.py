ec11f0abaac1346cbc5dcc1b41c430b575d71837
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-29 00:01:32 -0300

    Create ex071.py

---
+++ ex071.py
@@ -0,0 +1,8 @@
+# pythonexercios
+# Exercício 71 – Simulador de Caixa Eletrônico
+"""
+Crie um programa que simule o funcionamento de um caixa eletrônico.
+No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro)
+e o programa vai informar quantas cédulas de cada valor serão entregues.
+OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+"""
