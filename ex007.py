763e662c1b1afdb8d1cb2cdb02c07df43b9e53ff
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-28 21:37:24 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,3 +1,9 @@
+# pythonexercios
+# Exercício 7 – Média Aritmética
+"""
+Desenvolva um programa que leia as duas notas de um aluno,
+calcule e mostre a sua média.
+"""
 n1 = float(input('Vamos somar as notas\nDigite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 media = (n1 + n2) / 2
