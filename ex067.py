ed73bc5bc0726d426f50ba8db9dbb7335d529f27
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-30 21:49:49 -0300

    Update ex067.py

--- ex067.py
+++ ex067.py
@@ -5,3 +5,13 @@ Faça um programa que mostre a tabuada de vários números,
 um de cada vez, para cada valor digitado pelo usuário.
 O programa será interrompido quando o número solicitado for negativo.
 """
+var = 1
+print('Digete um número para saber a Tabuada.')
+print('Quando quiser para digite um número negativo.\n')
+while var > 0:
+    print('')
+    var = int(input('Digite um número:'))
+    print('A Tabuada para o numero {} é:\n'.format(var))
+    for c in range(1, 11):
+        print('{} x {:2} = {:3}'.format(var, c, c*var))
+print('fim do programa!')
