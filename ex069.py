fdb55965126cfc5ef09bf5a2a74131cfb6765b9c
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-31 20:55:56 -0300

    Exercício 69 – Análise de dados do grupo

--- ex069.py
+++ ex069.py
@@ -8,3 +8,30 @@ A) quantas pessoas tem mais de 18 anos.
 B) quantos homens foram cadastrados.
 C) quantas mulheres tem menos de 20 anos.
 """
+idade = []
+sexo = []
+c = 0
+men = 0
+womam = 0
+while 1 != 2:
+    i_sexo = str(input('Digite o sexo.\n[ M ] - masculino\n[ F ] - feminino\n')).upper()
+    sexo.append(i_sexo)
+    i_idade = int(input('Qual é a idade da pessoa: '))
+    idade.append(i_idade)
+    c = str(input('Você deseja continuar. S/N')).upper()
+    if c == 'S':
+        print('')
+        continue
+    if c == 'N':
+        break
+c = 0
+for i in range(0, len(idade)):
+    if idade[i] >= 18:
+        c += 1
+    if sexo[i] == 'M':
+        men += 1
+    if sexo[i] == 'F' and idade[i] <= 20:
+        womam += 1
+print('A {} pessoas maiores de 18 anos.'.format(c))
+print('E foram cadastrados {} homens no processo.'.format(men))
+print('E {} mulheres tem menos de 20 anos.'.format(womam))
