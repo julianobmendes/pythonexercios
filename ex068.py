8adeb9a332d23577e7f1b769f6325c3c7e68297a
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-10-30 23:45:55 -0300

    Exercício 68 – Jogo do Par ou Ímpar

--- ex068.py
+++ ex068.py
@@ -5,3 +5,20 @@ Faça um programa que jogue par ou ímpar com o computador.
 O jogo só será interrompido quando o jogador perder,
 mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
 """
+from random import randint
+cont = 0
+winner = ['Par','Ímpar']
+print('Vamos jogar par ou ímpar!\n[ 1 ] - Ímpar\n[ 2 ] - Par')
+while cont != 999:
+    num = int(input('Escolha sua sorte: '))
+    comp = randint(1, 2)
+    resp = (comp + num) % 2
+    cont += 1
+    if resp == num:
+        print('Deu {}, você ganhou!'.format(winner[resp]))
+        print('')
+    if resp != num:
+        print('')
+        break
+print('Após {} tentativa você não resistiu!!!'.format(cont))
+print('Deu {}, você perdeu!'.format(winner[resp]))
