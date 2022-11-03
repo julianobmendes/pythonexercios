181e6421fbba949c61f62f01df6ca52dc57a12db
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-02 23:45:54 -0300

    Exercício 71 – Simulador de Caixa Eletrônico

--- ex071.py
+++ ex071.py
@@ -6,3 +6,12 @@ No início, pergunte ao usuário qual será o valor a ser sacado (número inteir
 e o programa vai informar quantas cédulas de cada valor serão entregues.
 OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 """
+c = 50
+v = 20
+d = 10
+u = 1
+resto = 0
+while 1 != 2:
+    print('Bem vindo ao caixa eletrônico!')
+    saque = int(input('Quanto será o valor a ser sacado? '))
+    for i in range()
