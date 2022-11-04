d4c754c6033b2e0e2ee7ff70f1b221a1503ca487
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-03 22:15:15 -0300

    Create ex076.py

---
+++ ex076.py
@@ -0,0 +1,7 @@
+# pythonexercios
+# Exercício 76 – Lista de Preços com Tupla
+"""
+Crie um programa que tenha uma tupla única com nomes de produtos
+e seus respectivos preços, na sequência. No final,
+mostre uma listagem de preços, organizando os dados em forma tabular.
+"""
