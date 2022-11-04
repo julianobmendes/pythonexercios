fac4dd0f1188b2c8ed4ed1e408f211053e147ef8
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-03 22:37:30 -0300

    Create ex077.py

---
+++ ex077.py
@@ -0,0 +1,6 @@
+# pythonexercios
+# Exercício 77 – Contando vogais em Tupla
+"""
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
+Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+"""
