cfd3c860ca26933a005865117fdb83014202cdff
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-08 21:14:14 -0300

    Exercício 57 – Validação de Dados

--- ex057.py
+++ ex057.py
@@ -11,8 +11,9 @@ print('Para sexo feminino digite [F]')
 print('Para sair digite [0] zero')
 print('----------------------------------')
 while sexo != '0':
-    sexo = input('Digite o sexo da pessoa: ').upper()
+    sexo = input('Digite o sexo da pessoa: ').strip().upper()
     if sexo == 'M':
+        print(sexo)
         print('A pessoa é do sexo Masculino')
         print('----------------------------')
     elif sexo == 'F':
@@ -22,3 +23,9 @@ while sexo != '0':
         print('Você digitou um valor errado, favor repitir')
         print('-------------------------------------------')
 print('Fim do programa')
+"""
+sexo = str(input('Informe seu sexo: [M/F]')).strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: [M/F]')).strip().upper()[0]
+print('Sexo registrado com sucesso'.format(sexo))
+"""
