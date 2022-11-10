df46d74d051363181ed3e2111513789f5a58b018
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-10 19:44:01 -0300

    Exercício 65 – Maior e Menor valores

--- ex065.py
+++ ex065.py
@@ -7,20 +7,26 @@ qual foi o maior e o menor valores lidos.
 O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 """
 cont = []
-var = 0
-resp02 = 0
+var = resp02 = 0
 while var != 999:
     resp = int(input('Digite um número: '))
     if resp != 999:
         cont.append(resp)
+        resp02 = 0
         while resp02 != 999:
-            resp02 = str(input('Deseja continuar? S/N')).upper()
+            resp02 = str(input('---=== Deseja continuar? S/N ===---')).upper().strip()
             if resp02 == 'S':
                 resp02 = 999
                 continue
             if resp02 == 'N':
                 resp02 = 999
                 var = 999
-            if resp02 != 'S' and 'N':
+                continue
+            if resp02 != ('S') and resp02 != ('N'):
                 resp02 = 666
-                
+                print('Você digitou um valor errado!')
+print('')
+print('Você digitou {} vezes até o resultado final.'.format(len(cont)))
+print('O valor mínimo foi  de {}.'.format(min(cont)))
+print('O valor maxímo foi de {}.'.format(max(cont)))
+print('E a soma de todos os valores é {}'.format(sum(cont)))
