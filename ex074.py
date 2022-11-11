d8f562ac4650e8ab650321e66fa55bd897935c92
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-11 17:45:33 -0300

    Exercício 74 – Maior e menor valores em Tupla

--- ex074.py
+++ ex074.py
@@ -6,3 +6,13 @@ e colocar em uma tupla. Depois disso,
 mostre a listagem de números gerados e também indique o menor
 e o maior valor que estão na tupla.
 """
+from random import randint
+num = (randint(1,1000), randint(1,1000), randint(1,1000), randint(1,1000), randint(1,1000))
+c = 0
+print(('Os números sorteados foram'), end="")
+while c != len(num):
+    print((f', {num[c]}'), end="")
+    c += 1
+print('.')
+print(f'O maior número foi {max(num)}.')
+print(f'O menor número foi {min(num)}.')
