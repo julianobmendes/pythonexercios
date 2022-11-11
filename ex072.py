720d9a0c4c4037be0b4b1d18fb3fc4b419a1a355
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-10 22:14:55 -0300

    Exercício 72 – Número por Extenso

--- ex072.py
+++ ex072.py
@@ -6,3 +6,12 @@ com uma contagem por extenso, de zero até vinte.
 Seu programa deverá ler um número pelo teclado (entre 0 e 20)
 e mostrá-lo por extenso.
 """
+cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
+'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Desesseis', 'Desessete', 'Dezoito', 'Dezenove', 'Vinte')
+while True:
+    print('Digite um número de 0 a 20:')
+    num = int(input('Digite o número que você quer ver? '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente!\n')
+print(f'O número {num} por extenço é {cont[num]}')
