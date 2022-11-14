828c4ac68a5ece9dee649c51b0ba94e6937b0ab1
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-14 18:05:53 -0300

    Exercício 78 – Maior e Menor valores na Lista

--- ex078.py
+++ ex078.py
@@ -5,3 +5,9 @@ Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 No final, mostre qual foi o maior e o menor valor digitado e
 as suas respectivas posições na lista.
 """
+lista = []
+for c in range(0, 5):
+    leia = int(input('Digite um valor: '))
+    lista.append(leia)
+print(f'O maior valor é {max(lista)} e ele está na posições {(lista.index(max(lista)))+1}')
+print(f'O menor valor é {min(lista)} e ele está na posições {(lista.index(min(lista)))+1}')
