74c0d08d5c6cc8ee0a2264a1c119c0adc3c9e91a
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-16 23:00:39 -0300

    Create ex086.py

---
+++ ex086.py
@@ -0,0 +1,7 @@
+# pythonexercios
+# Exercício 86 – Matriz em Python
+"""
+Crie um programa que declare uma matriz de dimensão 3×3 e
+preencha com valores lidos pelo teclado. No final,
+mostre a matriz na tela, com a formatação correta.
+"""
