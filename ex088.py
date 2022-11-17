158d62b0c62e2149b9bf377d8290a1f8164925aa
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-16 23:03:09 -0300

    Create ex088.py

---
+++ ex088.py
@@ -0,0 +1,8 @@
+# pythonexercios
+# Exercício 88 – Palpites para a Mega Sena
+"""
+Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
+O programa vai perguntar quantos jogos serão gerados e vai
+sortear 6 números entre 1 e 60 para cada jogo,
+cadastrando tudo em uma lista composta.
+"""
