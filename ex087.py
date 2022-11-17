efc30a26353fd4048a8a77437a7749751a3131f9
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-16 23:01:59 -0300

    Create ex087.py

---
+++ ex087.py
@@ -0,0 +1,8 @@
+# pythonexercios
+# Exercício 87 – Mais sobre Matriz em Python
+"""
+Aprimore o desafio anterior, mostrando no final: 
+A) A soma de todos os valores pares digitados.
+B) A soma dos valores da terceira coluna.
+C) O maior valor da segunda linha.
+"""
