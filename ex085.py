bbc68b1cfb760a0c2d4446555616f078ce6c04e8
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-16 22:55:24 -0300

    Create ex085.py

---
+++ ex085.py
@@ -0,0 +1,7 @@
+# pythonexercios
+# Exercício 85 – Listas com pares e ímpares
+"""
+Crie um programa onde o usuário possa digitar sete valores numéricos e
+cadastre-os em uma lista única que mantenha separados os valorespares e ímpares.
+No final, mostre os valores pares e ímpares em ordem crescente.
+"""
