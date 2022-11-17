0ee0f5a234670dd08290062164406274dc6ff8c6
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-16 22:51:43 -0300

    Create ex084.py

---
+++ ex084.py
@@ -0,0 +1,9 @@
+# pythonexercios
+# Exercício 84 – Lista composta e análise de dados
+"""
+Faça um programa que leia nome e peso de várias pessoas,
+guardando tudo em uma lista. No final, mostre:
+A) Quantas pessoas foram cadastradas.
+B) Uma listagem com as pessoas mais pesadas.
+C) Uma listagem com as pessoas mais leves.
+"""
