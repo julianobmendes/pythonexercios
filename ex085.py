c87e541e9cdfe70b11a2db39be353e3575d63aa6
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-21 20:51:38 -0300

    Exercício 85 – Listas com pares e ímpares

--- ex085.py
+++ ex085.py
@@ -1,26 +1,24 @@
-
 # pythonexercios
 # Exercício 85 – Listas com pares e ímpares
 """
 Crie um programa onde o usuário possa digitar sete valores numéricos e
-cadastre-os em uma lista única que mantenha separados os valorespares e ímpares.
+cadastre-os em uma lista única que mantenha separados os valores pares e ímpares.
 No final, mostre os valores pares e ímpares em ordem crescente.
 """
 list_all = [[], []]
 for c in range(0, 7):
     ler = int(input(f'Digite o {c+1}° valor: '))
-    #if len(list_all) == 0:
-    #    if ler 
-    #    list_all.append(ler)
-    for i in range(0, len(list_all)):
-        if len(list_all) != 0 and ler % 2 == 0:
-            if list_all[i] > ler and ler not in list_all:
-                list_all[0].insert(i, ler)
-            if list_all[i] < ler and ler not in list_all:
-                list_all[0].append(ler)
-        if len(list_all) != 0 and ler % 2 == 1:
-            if list_all[i] > ler and ler not in list_all:
-                list_all[1].insert(i, ler)
-            if list_all[i] < ler and ler not in list_all:
-                list_all[1].append(ler)
-print(list_all)
+    if ler % 2 == 0:
+        list_all[0].append(ler)
+    if ler % 2 == 1:
+        list_all[1].append(ler)
+list_all[0].sort()
+list_all[1].sort()
+print('Os números pares são:', end="")
+for c in range(0, len(list_all[0])):
+    print(f' {list_all[0][c]}', end="")
+print('.')
+print('Os números ímpares são:', end="")
+for c in range(0, len(list_all[1])):
+    print(f' {list_all[1][c]}', end="")
+print('.')
