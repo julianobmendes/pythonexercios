f3ed647cdf963d97d9dff38d00f8af9179ffaf99
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-21 21:24:56 -0300

    Exercício 86 – Matriz em Python

--- ex086.py
+++ ex086.py
@@ -5,3 +5,10 @@ Crie um programa que declare uma matriz de dimensão 3×3 e
 preencha com valores lidos pelo teclado. No final,
 mostre a matriz na tela, com a formatação correta.
 """
+matriz = [[], [], []]
+for c in range(0, 3):
+    for i in range(0, 3):
+        num = int(input(f'Digite o {i+1}º número da linha {c+1}'))
+        matriz[c].append(num)
+for c in range(0, 3):
+    print(f'[ {matriz[c][0]} ] [ {matriz[c][1]} ] [ {matriz[c][2]} ]')
