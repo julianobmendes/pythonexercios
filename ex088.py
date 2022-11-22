50aeb75631e23089255c6e0b4f28b82dc18d4b3d
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-21 23:25:03 -0300

    Exercício 88 – Palpites para a Mega Sena

--- ex088.py
+++ ex088.py
@@ -6,3 +6,17 @@ O programa vai perguntar quantos jogos serão gerados e vai
 sortear 6 números entre 1 e 60 para cada jogo,
 cadastrando tudo em uma lista composta.
 """
+from random import randint
+quant = int(input('Quantos jogos você quer gerar? '))
+mega = [[] for _ in range(quant)]
+for c in range(0, len(mega)):
+    while True:
+        num = randint(1, 60)
+        if num not in mega[c]:
+            mega[c].append(num)
+        if len(mega[c]) == 6:
+            mega[c].sort()
+        if len(mega[c]) == 6:
+            break
+for c in range(0, len(mega)):
+    print(f'{mega[c]}')
