7835a523f54d49f1cbaba2ad17ca050daf2d6866
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-23 21:16:09 -0300

    Create ex091.py

---
+++ ex091.py
@@ -0,0 +1,8 @@
+# pythonexercios
+# Exercício 91 – Jogo de Dados em Python
+"""
+Crie um programa onde 4 jogadores joguem um dado e tenham
+resultados aleatórios. Guarde esses resultados em um
+dicionário em Python. No final, coloque esse dicionário
+em ordem, sabendo que o vencedor tirou o maior número no dado.
+"""
