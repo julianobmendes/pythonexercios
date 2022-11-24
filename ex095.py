10b23dece619926748a2b1c737bc6cd401e36dd5
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-23 21:46:21 -0300

    Create ex095.py

---
+++ ex095.py
@@ -0,0 +1,7 @@
+# pythonexercios
+# Exercício 95 – Aprimorando os Dicionários
+"""
+Aprimore o desafio 93 para que ele funcione com vários jogadores,
+incluindo um sistema de visualização de detalhes do
+aproveitamento de cada jogador.
+"""
