13094b63f4d7003325b3aaa8be594c8b797f5ff7
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-27 00:47:28 -0300

    Update ex094.py

--- ex094.py
+++ ex094.py
@@ -10,3 +10,25 @@ B) A média de idade
 C) Uma lista com as mulheres
 D) Uma lista de pessoas com idade acima da média
 """
+lista = list()
+while True:
+    nome = str(input('Nome: '))
+    test = 0
+    while test != 999:
+        sexo = str(input('Sexo? [ M/F]' )).upper()
+        if sexo == 'M':
+            sexo = 'Masculino'
+            test = 999
+        elif sexo == 'F':
+            sexo = 'Feminino'
+            test = 999
+        else:
+            print('Você selecionou um sexo errado!!!')
+    idade = int(input('Idade: '))
+    lista.append({'nome': nome, 'sexo': sexo, 'idade': idade})
+    resp = str(input('--== Continuar? [S/N]')).upper()
+    if resp == 'N':
+        break
+print(f'O grupo de pesquisa tem {len(lista)} pessoas.')
+print(sum(lista))
+print(lista)
