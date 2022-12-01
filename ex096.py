4d46660bcc40a74d6aea0dd38c503801a32ba0c0
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-30 21:27:40 -0300

    Create ex096.py

---
+++ ex096.py
@@ -0,0 +1,7 @@
+# pythonexercios
+# Exercício 96 – Função que calcula área
+"""
+Faça um programa que tenha uma função chamada área(),
+que receba as dimensões de um terreno retangular
+(largura e comprimento) e mostre a área do terreno.
+"""
