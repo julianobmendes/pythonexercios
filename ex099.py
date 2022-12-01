a68852953671854e577f690de1c02a423bf1719a
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-30 21:59:49 -0300

    Create ex099.py

---
+++ ex099.py
@@ -0,0 +1,7 @@
+# pythonexercios
+# Exercício 99 – Função que descobre o maior
+"""
+Faça um programa que tenha uma função chamada maior(),
+que receba vários parâmetros com valores inteiros.
+Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+"""
