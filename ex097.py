b638929a958b62aeb56b16e890982173e4bdc11d
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-30 21:46:02 -0300

    Create ex097.py

---
+++ ex097.py
@@ -0,0 +1,13 @@
+# pythonexercios
+# Exercício 97 – Um print especial
+"""
+Faça um programa que tenha uma função chamada escreva(),
+que receba um texto qualquer como parâmetro e
+mostre uma mensagem com tamanho adaptável.
+Ex: 
+escreva('Olá, Mundo!')
+Saída:
+~~~~~~~~~
+ Olá, Mundo!
+~~~~~~~~~
+"""
