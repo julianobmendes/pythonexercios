a302dd0633a12f296a66ae0ad585d9b8058d8258
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-11-30 21:09:06 -0300

    Exercício 95 – Aprimorando os Dicionários

--- ex095.py
+++ ex095.py
@@ -14,9 +14,10 @@ while True:
     part = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
     for c in range(0, part):
         gols.append(int(input(f'Quantos gols na {c+1}º partida?')))
-    jogador['gols'] = gols
+    jogador['gols'] = gols[:]
     jogador['total'] = sum(gols)
     lista.append(jogador.copy())
+    gols.clear()
     while True:
         resp = str(input('--== Continuar? [S/N]')).upper()[0]
         if resp in 'SN':
@@ -26,14 +27,27 @@ while True:
     if resp == 'N':
         break
 print('-='*20)
-print('Cod.', end="")
+print('Cod. ', end="")
 for c in jogador.keys():
     print(f'{c:<15}', end="")
 print()
 print('-'*40)
 for k, v in enumerate(lista):
-    print(f'{k:>3}', end="")
+    print(f'{k:>3} ', end="")
     for c in v.values():
-        print(f'{str(c):<15}', end="")
+        print(f' {str(c):<14}', end="")
     print()
 print('-='*20)
+while True:
+    print('Para encerrar digite "999"')
+    part = int(input('Qual jogador quer ver os dados? '))
+    if part == 999:
+        break
+    print('-'*47)
+    print(f'---LEVANTAMENTO DO JOGADOR: {lista[part]["nome"]}')
+    for c in range(0, len(lista[part]["gols"])):
+        print(f'      No jogo {c+1}, fez {lista[part]["gols"][c]} gols.')
+    print(f'- Fez um total de {lista[part]["total"]} gols.')
+    print(f'- E com uma media de gols de {lista[part]["total"] / len(lista[part]["gols"])} por partida.')
+    print('-'*47)
+print('Fim do programa.')
