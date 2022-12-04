b1183c851ab85253aaeb4e87bdf3907602dba3fc
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-12-03 21:29:16 -0300

    Create ex104.py

---
+++ ex104.py
@@ -0,0 +1,9 @@
+# pythonexercios
+# Exercício 104 – Validando entrada de dados em Python
+"""
+Crie um programa que tenha a função leiaInt(),
+que vai funcionar de forma semelhante
+‘a função input() do Python, só que fazendo a
+validação para aceitar apenas um valor numérico.
+Ex: n = leiaInt(‘Digite um n: ‘)
+"""
