33c9ab4c59fef7baa78c00a0cddc3ccdcdbd68c9
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-12-05 21:15:14 -0300

    Exercício 101 – Funções para votação

--- ex101.py
+++ ex101.py
@@ -6,3 +6,18 @@ que vai receber como parâmetro o ano de nascimento de uma pessoa,
 retornando um valor literal indicando se uma pessoa tem voto
 NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 """
+from datetime import date
+def voto(ano):
+    idade = (date.today().year - ano)
+    print(f'Com {idade} anos: ', end="")
+    if idade >= 65:
+        print('Voto OPCIONAL')
+    if idade >= 18 and idade <= 64:
+        print('Voto OBRIGATÓRIO')
+    if idade >= 16 and idade <= 17:
+        print('Voto OPCIONAL')
+    if idade <= 15:
+        print('Voto NEGADO')
+
+
+voto(int(input('Em que ano você nasceu? ')))
