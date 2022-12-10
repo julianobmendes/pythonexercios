710328f2dc9e4650fb0ebcf65dbd8c0ebfef69a5
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-12-10 18:07:16 -0300

    Exercício 102 – Função para Fatorial

--- ex102.py
+++ ex102.py
@@ -7,3 +7,15 @@ o primeiro que indique o número a calcular e outro chamado show,
 que será um valor lógico (opcional) indicando se será mostrado ou
 não na tela o processo de cálculo do fatorial.
 """
+def fatorial(num02, show=False):
+    num01 = num02
+    print(f'fatorial de {num01}! = {num02}'if show == True else f'fatorial de {num01}!', end="")
+    while num02 != 1:
+        num02 -= 1
+        num01 *= num02
+        if show == True:
+            print(f' x {num02}', end="")
+    print(f' = {num01}')
+
+
+fatorial(5, True)
