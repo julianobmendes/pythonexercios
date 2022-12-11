75d1b0112f440b8c6d9e1d25ce864f151c055e1f
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-12-11 20:36:25 -0300

    Exercício 103 – Ficha do Jogador

--- ex103.py
+++ ex103.py
@@ -7,3 +7,17 @@ jogador e quantos gols ele marcou.
 O programa deverá ser capaz de mostrar a ficha do jogador,
 mesmo que algum dado não tenha sido informado corretamente.
 """
+def ficha(nome='<Desconhecido>', gols=0):
+    print(f'O jogador {nome} fez {gols} gols no campeonato.')
+
+
+nome = str(input('Qual o nome: '))
+gols = str(input('Quantos gols: '))
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+    ficha(gols=gols)
+else:
+    ficha(nome, gols)
