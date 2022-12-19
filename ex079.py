56a97e1027698bee73a073c16bfcdefd9cf087bb
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2022-12-19 20:58:48 -0300

    Exercício 79 – Valores únicos em uma Lista

--- ex079.py
+++ ex079.py
@@ -6,16 +6,19 @@ e cadastre-os em uma lista. Caso o número já exista lá dentro,
 ele não será adicionado. No final, serão exibidos todos os valores
 únicos digitados, em ordem crescente.
 """
-lista = []
+lista = list()
 while True:
     ler  = int(input('digite um valor'))
-    if ler == 0:
-        break
     if ler not in lista:
         lista.append(ler)
+        print('Item adicionado...')
+    else:
+        print('Valor repetido não adicionado!!!')
+    resp = str(input('-== Quer continuar??? ==- [S/N]')).upper()
+    if resp in 'N':
+        break
 lista.sort()
 print(f'Os valores digitados foram:', end="")
 for c in range(0, len(lista)):
     print(f' {lista[c]}'if c == 0 else f', {lista[c]}', end="")
 print('.')
- 
