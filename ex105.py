45122b7aa1c2bfb1d257280b452849740966ca8b
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-03 00:00:19 -0300

    Exercício 105 – Analisando e gerando Dicionários

--- ex105.py
+++ ex105.py
@@ -4,24 +4,22 @@
 Faça um programa que tenha uma função notas() que pode receber
 várias notas de alunos e vai retornar um
 dicionário com as seguintes informações:
-
 - Quantidade de notas
 - A maior nota
 - A menor nota
 - A média da turma
 - A situação (opcional)
-
 Adicione também as docstrings dessa função para
 consulta pelo desenvolvedor.
 """
-def notas(**num, sit=False):
+def notas(* num, sit=False):
     """
-
     :param num:
     :param sit:
     :return:
     """
-    print(f'A maior nota foi: {max(num)}')
-    print(f'A menor nota foi: {min(num)}')
-    print(f'A média da turma: {sum(num) / len(num)}')
-    print(f'Situação: {}')
+    lista = {'total': len(num),'maior'max(num), 'menor': min(num), 'media': sum(num) / len(num)}
+    print(lista)
+    
+    
+notas(5, 6, 5.5, 4)
