d948140c9231c18c421eac0a40b4f3286619a2aa
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-13 00:07:19 -0300

    Exercício 5 – Antecessor e Sucessor

--- ex005.py
+++ ex005.py
@@ -5,4 +5,4 @@ Faça um programa que leia um número Inteiro e
 mostre na tela o seu sucessor e seu antecessor.
 """
 n1 = int(input('Digite um numero inteiro! '))
-print('O numero inteiro é {}, e seu antecessor é {} e seu predecessor é {}'.format(n1, n1-1, n1+1))
+print('O numero inteiro é {}, e seu antecessor é {} e seu sucessor é {}'.format(n1, n1-1, n1+1))
