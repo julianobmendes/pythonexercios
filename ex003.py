96e1d19e8f6b0045f462726db9b2f671c86c2b6d
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-12 23:54:37 -0300

    Exercício 3 – Somando dois números

--- ex003.py
+++ ex003.py
@@ -3,7 +3,7 @@
 """
 Crie um programa que leia dois números e mostre a soma entre eles.
 """
-n1 = int(input('Digite um numero! '))
-n2 = int(input('Digite outro numero! '))
-s = n1 + n2
-print('O valor de {}{} e {} {}somados é {}{}{}'.format('\033[1;30m', n1, n2, '\033[m', '\033[7;30m', s, '\033[m'))
+n1 = int(input('Digite um número! '))
+n2 = int(input('Digite outro número! '))
+soma = n1 + n2
+print('O valor de {}{} e {} {}somados é {}{}{}'.format('\033[1;30m', n1, n2, '\033[m', '\033[7;30m', soma, '\033[m'))
