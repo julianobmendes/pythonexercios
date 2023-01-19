8470f5fe9c0dc99ca9e11b6882c1061432882f13
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-19 17:48:19 -0300

    Função ex109

--- ex109/moeda/__init__.py
+++ ex109/moeda/__init__.py
@@ -1,4 +1,4 @@
-def aumentar(num, porc, show=False):
+def aumentar(num=0, porc=0, show=False):
     """
     -> Função de soma porcentagem ao valor.
     :param num: valor a ser aumentado.
@@ -7,11 +7,11 @@ def aumentar(num, porc, show=False):
     :param show: define se o valor vai aparecer formatado em formato contábil
     :return: 'resp': resposta
     """
-    resp = (porc * num) / 100
-    return f'R$ {num + resp:.2f}' if show else num + resp
+    resp = num + ((porc * num) / 100)
+    return f'R$ {moeda(resp)}' if show else num + resp
 
 
-def diminuir(num, porc, show=False):
+def diminuir(num=0, porc=0, show=False):
     """
     -> Função de retirar porcentagem ao valor.
     :param num: valor a ser diminuído.
@@ -20,11 +20,11 @@ def diminuir(num, porc, show=False):
     :param show: define se o valor vai aparecer formatado em formato contábil
     :return: 'resp': resposta
     """
-    resp = (porc * num) / 100
-    return f'R$ {num - resp:.2f}' if show else num - resp
+    resp = num - ((porc * num) / 100)
+    return f'R$ {moeda(resp)}' if show else num - resp
 
 
-def dobro(num, show=False):
+def dobro(num=0, show=False):
     """
     -> Função para dobra o valor.
     :param num: recebe o valor a ser somado.
@@ -33,10 +33,10 @@ def dobro(num, show=False):
     :return: 'resp': resposta
     """
     resp = num + num
-    return f'R$ {resp:.2f}' if show else resp
+    return f'R$ {moeda(resp)}' if show else resp
 
 
-def metade(num, show=False):
+def metade(num=0, show=False):
     """
     -> Função para dividir um valor.
     :param num: recebe o valor a ser dividido.
@@ -45,15 +45,15 @@ def metade(num, show=False):
     :return: 'resp': resposta
     """
     resp = num / 2
-    return f'R$ {resp:.2f}' if show else resp
+    return f'R$ {moeda(resp)}' if show else resp
 
 
-def moeda(num):
+def moeda(num=0):
     """
     -> Função para formatar em valor monetário.
     :param num: recebe o número a ser formatado.
     :param resp: formata 'num' em monetário com duas casas decimais
     :return: 'resp': resposta
     """
-    resp = f'R$ {num:.2f}'
+    resp = f'R$ {num:.2f}'.replace('.', ',')
     return resp
