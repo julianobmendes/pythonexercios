bd3ca8568d807a586a652c0ce0d31aaf3df85d19
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-19 21:24:49 -0300

    Exercício 111 – Transformando módulos em pacotes

--- ex111.py
+++ ex111.py
@@ -6,3 +6,7 @@ internos chamados moeda e dado. Transfira todas as funções
 utilizadas nos desafios 107, 108 e 109 para o primeiro pacote
 e mantenha tudo funcionando.
 """
+from ex11.utilidadesCeV import moeda
+
+p = float(input('Digite o preço: R$ '))
+moeda.resumo(p, 20, 13)
