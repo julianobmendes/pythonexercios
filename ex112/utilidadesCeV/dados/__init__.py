15e89665b824203ef5a24170c6a7b99070a7ec9b
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-20 00:04:41 -0300

    Update __init__.py

--- ex112/utilidadesCeV/dados/__init__.py
+++ ex112/utilidadesCeV/dados/__init__.py
@@ -1,1 +1,19 @@
-
+def leiaInt(msg):
+    """
+    obs. lê em str e transforma em int
+    :param msg: número a ser verificado
+    :return: valor númerico altera para int
+    by Juliano Boaventura Mendes
+    """
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31m ERRO!!! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
