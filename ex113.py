611a1c3d1eb26e2e62bcff2891a6ed1a4137e4a9
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-23 20:27:38 -0300

    Create ex113.py

---
+++ ex113.py
@@ -0,0 +1,9 @@
+# pythonexercios
+# Exercício 113 – Funções aprofundadas em Python
+"""
+Reescreva a função leiaInt() que fizemos no desafio 104,
+incluindo agora a possibilidade da digitação de um número
+de tipo inválido.
+Aproveite e crie também uma função leiaFloat()
+com a mesma funcionalidade.
+"""
