23c7641def2403f809d3dbe022cc926ea67169ab
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-23 19:47:30 -0300

    Exercício 112 – Entrada de dados monetários

--- ex112.py
+++ ex112.py
@@ -6,5 +6,6 @@ temos um módulo chamado dado. Crie uma função chamada leiaDinheiro()
 que seja capaz de funcionar como a função imputa(),
 mas com uma validação de dados para aceitar apenas valores que seja monetários.
 """
-p = leiaDinheiro('Digite o preço: R$ ')
+from ex112.utilidadesCeV import dados
+p = dados.leiaDinheiro('Digite o preço: R$ ')
 print(p)
