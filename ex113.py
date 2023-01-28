74c7e2b7a757d75c4a07f52a541fdcaa12d1fbf2
julianobmendes <62814927+julianobmendes@users.noreply.github.com>
2023-01-28 00:29:49 -0300

    Update ex113.py

--- ex113.py
+++ ex113.py
@@ -7,3 +7,35 @@ de tipo inválido.
 Aproveite e crie também uma função leiaFloat()
 com a mesma funcionalidade.
 """
+
+def leiaint(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('ERROR: Valor digitado não é um número inteiro!')
+            continue
+        except (KeyboardInterrupt):
+            print('ERROR: Usuário preferiu não digitar este número! ')
+            return 0
+        else:
+            return n
+
+
+def leiafloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('ERROR: Valor digitado não é um número real!')
+            continue
+        except (KeyboardInterrupt):
+            print('ERROR: Usuário preferiu não digitar este número! ')
+            return 0
+        else:
+            return n
+
+
+n1 = leiaint('Digite um número inteiro: ')
+n2 = leiafloat('Digite um número real: ')
+print(f'O número inteiro foi {n1} e o real foi {n2}.')
